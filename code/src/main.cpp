18ac5db724e4552cf66bd69a0a90146a1ded2308
Amy While <26681721+elihwyma@users.noreply.github.com>
2025-02-24 11:40:36 +0000

    feat: command line args

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -1,12 +1,35 @@
 #include <stdio.h>
 #include <interpreter.h>
+#include <iostream>
+#include <string>
 #include <opencv2/opencv.hpp>
 
-Interpreter interpreter("resources/face_landmark.tflite");
-cv::VideoCapture cameraCapture(0);
-
-int main() {
+int main(int argc, char* argv[]) {
     printf("Hello, World!\n");
 
+    std::string modelPath = "resources/face_landmark.tflite";
+    int cameraID = 0;
+
+    for (int i = 1; i < argc - 1; i++) {
+        if (strcmp(argv[i], "-m") == 0) {
+
+            modelPath = argv[i + 1];
+        }
+        if (strcmp(argv[i], "-v") == 0) {
+            try {
+                cameraID = atoi(argv[i + 1]);
+            } catch(...) {
+                // 
+            }
+        }
+    }
+
+    cv::VideoCapture cameraCapture(cameraID);
+    Interpreter interpreter(modelPath.c_str());
+
+    if (!cameraCapture.isOpened()) {
+        std::cerr << "Error opening camera." << std::endl;
+        return -1;
+    }
     return 0;
 }
